dde04aee9ab7d37b2ca8f653782851355181c9e5
Luciggl <lucielionascimento19@gmail.com>
2024-01-05 00:43:36 -0300

    atualizando os caminhos de path

--- src/main/java/Model/repositories/Path.java
+++ src/main/java/Model/repositories/Path.java
@@ -4,4 +4,6 @@ public interface Path {
     String pathEstoque = "src/main/java/Model/BDEstoque/bdEstoque.txt";
     String pathTransacao = "src/main/java/Model/BDEstoque/bdTransações.txt";
     String pathUsers = "src/main/java/Model/BDEstoque/bdUsers.txt";
+
+    String pathMovimentacao = "src/main/java/Model/BDEstoque/bdMovimentações";
 }

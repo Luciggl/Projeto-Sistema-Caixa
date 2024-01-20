2c4126af8ff3b42a74ab9694d1ae205fbfdf108f
Luciggl <lucielionascimento19@gmail.com>
2024-01-20 20:53:28 -0300

    docs: implementando a documentação da interface de variaveis importantes

--- src/main/java/Model/utils/Path.java
+++ src/main/java/Model/utils/Path.java
@@ -1,8 +1,28 @@
 package Model.utils;
 
+/**
+ * A interface Path fornece caminhos para arquivos no sistema, especificamente relacionados ao armazenamento de dados.
+ * Este pacote oferece acesso aos caminhos dos arquivos de banco de dados para diferentes funcionalidades do sistema.
+ */
 public interface Path {
+
+    /**
+     * Caminho para o arquivo de banco de dados do estoque.
+     */
     String pathEstoque = "src/main/java/Model/BD/bdEstoque.txt";
+
+    /**
+     * Caminho para o arquivo de banco de dados de transações.
+     */
     String pathTransacao = "src/main/java/Model/BD/bdTransações.txt";
+
+    /**
+     * Caminho para o arquivo de banco de dados de usuários.
+     */
     String pathUsers = "src/main/java/Model/BD/bdUsers.txt";
+
+    /**
+     * Caminho para o arquivo de banco de dados de movimentações.
+     */
     String pathMovimentacao = "src/main/java/Model/BD/bdMovimentações.txt";
 }

58f7f4402705f1685194683a6fdfc96766b4dd63
Luciggl <lucielionascimento19@gmail.com>
2024-02-29 10:57:11 -0300

    Melhorando o sistema de Impressão dos dados

--- src/main/java/Model/utils/Text.java
+++ src/main/java/Model/utils/Text.java
@@ -1,2 +1,16 @@
-package Model.utils;public class Text {
+package Model.utils;
+
+import javax.swing.*;
+
+public class Text {
+    public static void Imprimir(String msg, String title){
+        JTextArea textArea = new JTextArea(15, 30);
+        textArea.setText(msg);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        JOptionPane.showMessageDialog(null, scrollPane, title, JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public static boolean verificarSeNaoEstaVazioNull(String validar) {
+        return validar != null && !validar.isEmpty();
+    }
 }
